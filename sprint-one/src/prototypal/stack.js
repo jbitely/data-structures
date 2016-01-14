06322a447b60a98eaeccb70eedd905db192ef3c1
Justin Bitely <jbitely@gmail.com>
2016-01-14 18:19:00 -0500

    Completed stack.js prototypal pattern; all tests pass.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,7 +1,23 @@
 var Stack = function(){
+  var stack = Object.create(stackMethods);
+  stack.stackSize = 0;
+  return stack;
+};
 
-}
-
-var stackMethods = {};
-
-
+var stackMethods = {
+  size : function(){
+   return this.stackSize;
+  },
+  push: function(value){
+    this[this.stackSize] = value;
+    this.stackSize++;
+  },
+  pop: function(){
+    if(this.stackSize){
+      this.stackSize--;
+      var value = this[this.stackSize];
+      delete this[this.stackSize];
+      return value;
+    }
+  }
+};
