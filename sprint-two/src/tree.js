384b6ecce0e05b587bd7a5a33f4d132b86d0e530
Justin Bitely <jbitely@gmail.com>
2016-01-15 18:02:50 -0500

    Implemented basic .contains method logic for tree.js.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,15 +1,10 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
-
   newTree.children = [];
   _.extend(newTree, treeMethods);
   return newTree;
 };
-
-
-
-
 
 var treeMethods = {};
 
@@ -19,10 +14,14 @@ treeMethods.addChild = function(value){
 };
 
 treeMethods.contains = function(target){
+  console.log(this.value);
 
+  if (this.value === target) {
+    return true;
+  } else if (this.children) {
+    for (var i = 0; i < this.children.length; i++) {
+      return this.children[i].contains(target);
+    }
+  }
+  return false;
 };
-
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
