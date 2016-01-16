d2d76c0d208562084f0a75b4ec40920869f8e607
Justin Bitely <jbitely@gmail.com>
2016-01-16 14:38:35 -0500

    Implemented .remove in hashTable.js; refactored to handle collisions.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -8,25 +8,43 @@ HashTable.prototype.insert = function(k, v){
   var bucket = this._storage.get(i);
   if (!bucket) {
     bucket = [];
+    this._storage.set(i, bucket);
   }
-  this._storage.set(i, [k, v]);
-
+  var found = false;
+  for (var j = 0; j < bucket.length; j++) {
+    if (bucket[j][0] === k) {
+      bucket[j][1] = v;
+      found = true;
+    }
+  }
+  if (!found) {
+    bucket.push([k, v]);
+  }
+  console.log(bucket);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(i);
+  var result = null;
   for (var j = 0; j < bucket.length; j++) {
-    if (bucket[j] === k) {
-      return bucket[j + 1];
+    if (bucket[j][0] === k) {
+      result = true;
+      return bucket[j][1];
     }
   }
-
-
+  return result;
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  for (var j = 0; j < bucket.length; j++) {
+    if (bucket[j][0] === k) {
+      bucket.splice(j, 1);
+      return;
+    }
+  }
 };
 
 
